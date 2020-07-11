03021bdf7db983e72e00c71592e6319154681216
Wei Ding <ding_web@yahoo.co.jp>
2020-07-11 00:32:16 -0400

    update starter

--- Entrance/Starter/Setup.cs
+++ Entrance/Starter/Setup.cs
@@ -10,8 +10,16 @@ namespace Starter
 			Page page;
 
 			page = new Page() { Title = "Euler Problems 01-10" };
-			page.Problems.Add(new Problem { Type = typeof(MultiplesOf3And5), Method = "Start", Description = "Multiples Of 3 And 5" });
-			page.Problems.Add(new Problem { Type = typeof(EvenFibonacciNumbers), Method = "Start", Description = "Even Fibonacci Numbers" });
+			page.Problems.Add(new Problem {
+				Type = typeof(MultiplesOf3And5),
+				Method = "Start",
+				Description = "Multiples Of 3 And 5"
+			});
+			page.Problems.Add(new Problem {
+				Type = typeof(EvenFibonacciNumbers),
+				Method = "Start",
+				Description = "Even Fibonacci Numbers"
+			});
 			menu.Pages.Add(page);
 
 			page = new Page() { Title = "Euler Problems 11-20" };
