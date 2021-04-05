5dd7864bdbf5ce2ad709686721c39d6d79a49269
Wei Ding <ding_web@yahoo.co.jp>
2021-04-04 23:51:27 -0400

    compare strings

--- Assessment/Filtered/SameStrings.cs
+++ Assessment/Filtered/SameStrings.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Linq;
 
 namespace Assessment.Filtered {
 	public class SameStrings {
 		public void Start() {
 			Console.WriteLine("-- Two same strings");
+			Action("helloWorld", "oworldHell");
+			Action("The same string", "stringemas  heT");
+			Action("abcd", "abcf");
+		}
+
+		private void Action(string a, string b) {
+			Console.WriteLine($"[{a}]");
+			Console.WriteLine($"[{b}]");
+			Console.WriteLine($"-> {Compare(a,b)}");
+		}
+
+		private bool Compare(string a, string b) {
+			var lista = a.ToUpper().ToCharArray().ToList().OrderBy(x => x);
+			var listb = b.ToUpper().ToCharArray().ToList().OrderBy(x => x);
+			return Enumerable.SequenceEqual(lista, listb);
 		}
 	}
 }
