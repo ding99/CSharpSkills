fa7c6b24485697f1384f363fb687cd1643076b87
Wei Ding <ding_web@yahoo.co.jp>
2021-04-05 12:25:45 -0400

    more test

--- LeetCode/Leet01_10/MedianOfArrays.cs
+++ LeetCode/Leet01_10/MedianOfArrays.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Numbers;
+using System;
 using System.Collections.Generic;
-using System.Text;
-using Numbers;
 
 namespace LeetCode.Leet01_10 {
 	public class MedianOfArrays {
 		public void Start() {
 			Action(new int[] { 1, 3 }, new int[] { 2 });
+			Action(new int[] { 1, 2 }, new int[] { 3,4 });
+			Action(new int[] { 0, 0 }, new int[] { 0,0 });
+			Action(new int[0], new int[] {1 });
+			Action(new int[] { 2}, new int[0] );
 		}
 
 		private void Action(int[] a1, int[] a2) {
@@ -15,10 +18,9 @@ namespace LeetCode.Leet01_10 {
 			combine.AddRange(a2);
 			int[] sum = combine.ToArray();
 			Array.Sort(sum);
-			Console.WriteLine($"combined {IntArray.ToShow(sum)}");
 
 			int size = sum.Length;
-			double median = size % 2 == 0 ? (sum[size / 2 - 1] + sum[size / 2]) / 2.0 : sum[size / 2] * 1.0;
+			double median = size % 2 == 0 ? (sum[size / 2 - 1] + sum[size / 2]) / 2.0 : sum[size / 2];
 			Console.WriteLine("Output: {0:f}", median);
 		}
 
