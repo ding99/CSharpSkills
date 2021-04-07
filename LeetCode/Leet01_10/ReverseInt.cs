27b1981eaf54f30edc293faa281f965cec0cda66
Wei Ding <ding_web@yahoo.co.jp>
2021-04-07 15:23:29 -0400

    use minus in string

--- LeetCode/Leet01_10/ReverseInt.cs
+++ LeetCode/Leet01_10/ReverseInt.cs
@@ -8,11 +8,27 @@ using System;
 namespace LeetCode.Leet01_10 {
 	public class ReverseInt {
 		public void Start() {
+			testm();
 			Action(123);
 			Action(-123);
 			Action(120);
 			Action(0);
 			Action(2147483647);
+		}
+
+		private void testm() {
+			int max = Int32.MaxValue, min = Int32.MinValue;
+			Console.WriteLine($"max ({max}), min ({min})");
+
+			string maxs = max.ToString(), mins = min.ToString();
+			Console.WriteLine($"maxs ({maxs}), mins ({mins})");
+
+			int re = 0;
+			if (Int32.TryParse(maxs, out re)) Console.WriteLine($"parsed maxs ({re})");
+			if (Int32.TryParse(mins, out re)) Console.WriteLine($"parsed mins ({re})");
+
+			maxs += "1"; mins += "1";
+			Console.WriteLine($"trying parse longer maxs ({Int32.TryParse(maxs, out re)}), mins ({Int32.TryParse(mins, out re)}, re ({re}))");
 		}
 
 		private void Action(int x) {
@@ -21,10 +37,10 @@ namespace LeetCode.Leet01_10 {
 			char[] cx = Math.Abs(x).ToString().ToCharArray();
 			Array.Reverse(cx);
 			string nx = new string(cx);
+			if (minus) nx = "-" + nx;
 
-			int rev = 0;
-			if (Int32.TryParse(nx, out rev))
-				rev *= minus ? -1 : 1;
+			int rev;
+			Int32.TryParse(nx, out rev);
 
 			Console.WriteLine($"Ouput: {rev}");
 		}
