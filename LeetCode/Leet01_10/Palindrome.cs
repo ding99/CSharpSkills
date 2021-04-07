53f3e5a7650865aedc8605694a3a37446cc79e12
Wei Ding <ding_web@yahoo.co.jp>
2021-04-07 18:47:26 -0400

    more tests

--- LeetCode/Leet01_10/Palindrome.cs
+++ LeetCode/Leet01_10/Palindrome.cs
@@ -4,19 +4,20 @@ An integer is a palindrome when it reads the same backward as forward. For examp
 */
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Utilities.Characters;
 
 namespace LeetCode.Leet01_10 {
 	public class Palindrome {
 		public void Start() {
 			Action(121);
+			Action(-121);
+			Action(10);
+			Action(-101);
 		}
 
 		private void Action(int x) {
 			Console.WriteLine($"Input: x = {x}");
-			Console.WriteLine($"Output: {ManipulateStrings.IsPalindromic(x.ToString())}");
+			Console.WriteLine($"Output: {ManipulateStrings.IsPalindromic(x.ToString()).ToString().ToLower()}");
 		}
 	}
 }
