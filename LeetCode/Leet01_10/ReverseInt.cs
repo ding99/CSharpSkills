4dba3bd5fb699e256b877b9177c24279a01d79c1
Wei Ding <ding_web@yahoo.co.jp>
2021-04-07 15:24:33 -0400

    remove test method

--- LeetCode/Leet01_10/ReverseInt.cs
+++ LeetCode/Leet01_10/ReverseInt.cs
@@ -8,27 +8,11 @@ using System;
 namespace LeetCode.Leet01_10 {
 	public class ReverseInt {
 		public void Start() {
-			testm();
 			Action(123);
 			Action(-123);
 			Action(120);
 			Action(0);
 			Action(2147483647);
-		}
-
-		private void testm() {
-			int max = Int32.MaxValue, min = Int32.MinValue;
-			Console.WriteLine($"max ({max}), min ({min})");
-
-			string maxs = max.ToString(), mins = min.ToString();
-			Console.WriteLine($"maxs ({maxs}), mins ({mins})");
-
-			int re = 0;
-			if (Int32.TryParse(maxs, out re)) Console.WriteLine($"parsed maxs ({re})");
-			if (Int32.TryParse(mins, out re)) Console.WriteLine($"parsed mins ({re})");
-
-			maxs += "1"; mins += "1";
-			Console.WriteLine($"trying parse longer maxs ({Int32.TryParse(maxs, out re)}), mins ({Int32.TryParse(mins, out re)}, re ({re}))");
 		}
 
 		private void Action(int x) {
