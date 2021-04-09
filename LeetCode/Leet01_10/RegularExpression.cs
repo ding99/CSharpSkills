4d3204c9a91bafd10574e1ac33a76cba11ec319b
Wei Ding <ding_web@yahoo.co.jp>
2021-04-08 23:59:28 -0400

    regulaui exp

---
+++ LeetCode/Leet01_10/RegularExpression.cs
@@ -0,0 +1,24 @@
+﻿/*
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*' where: 
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Leet01_10 {
+	public class RegularExpression {
+		public void Start() {
+			Matching("aa", "a");
+			Matching("aa", "a*");
+		}
+
+		private void Matching(string s, string p) {
+			Console.WriteLine($"Input: s = \"{s}\", p = \"{p}\"");
+		}
+	}
+}
