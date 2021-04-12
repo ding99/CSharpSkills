ff0a870f09482655a3ffa1d2a3d19e3da3a3ddc1
Wei Ding <ding_web@yahoo.co.jp>
2021-04-12 18:36:12 -0400

    matched

--- LeetCode/Leet01_10/RegularExpression.cs
+++ LeetCode/Leet01_10/RegularExpression.cs
@@ -9,6 +9,7 @@ The matching should cover the entire input string (not partial).
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace LeetCode.Leet01_10 {
 	public class RegularExpression {
@@ -19,6 +20,21 @@ namespace LeetCode.Leet01_10 {
 
 		private void Matching(string s, string p) {
 			Console.WriteLine($"Input: s = \"{s}\", p = \"{p}\"");
+
+			Match m = Regex.Match(s, p.Replace(".", "\\w"));
+			Console.WriteLine($"matched value [{m.Value}]");
+			Console.WriteLine($"Output: {m.Value == s}");
+		}
+
+		private void Matchings(string s, string p) {
+			Regex r = new Regex(p.Replace(".", "\\w"));
+			MatchCollection m = r.Matches(s);
+			Console.WriteLine($"Input: s = \"{s}\", p = \"{p}\"");
+			Console.Write($"m count({m.Count})");
+			for (int i = 0; i < m.Count; i++)
+				Console.Write($" <{m[i].Index}>-<{m[i].Value}>");
+			Console.WriteLine();
+			Console.WriteLine($"Output: {m.Count == 1 && m[0].Value == s}");
 		}
 	}
 }
