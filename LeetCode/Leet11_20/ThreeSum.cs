38e2fd3cc692c9bc78e0131da77665bd606080be
Wei Ding <ding_web@yahoo.co.jp>
2021-04-13 14:47:32 -0400

    new class for 3sum

---
+++ LeetCode/Leet11_20/ThreeSum.cs
@@ -0,0 +1,21 @@
+﻿
+/*
+-- 3Sum
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets. 
+
+-- 3Sum Closer
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Leet11_20 {
+	public class ThreeSum {
+		public void SumZero() {
+
+		}
+	}
+}
