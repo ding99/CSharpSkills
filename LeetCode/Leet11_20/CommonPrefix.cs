d7b8a0a4cd21a5962009b6fe0f932217ceabb1ea
Wei Ding <ding_web@yahoo.co.jp>
2021-04-13 14:39:01 -0400

    whole test

--- LeetCode/Leet11_20/CommonPrefix.cs
+++ LeetCode/Leet11_20/CommonPrefix.cs
@@ -1,52 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+*/
+
+using System;
 using System.Linq;
 
 namespace LeetCode.Leet11_20 {
 	public class CommonPrefix {
 		public void Start() {
-			Common(new string[] { "flower","flow", "flight", "", "ab"});
-			Common(new string[] { "flower", "flow", "flight", "fab" });
+			Common(new string[] { "flower", "flow", "flight"});
+			Common(new string[] { "dog", "racecar", "car" });
 		}
 
 		private void Common(string[] strs) {
 			int size = strs.Length;
-			Console.Write("Input: strs = [");
-			for (int i = 0; i < size; i++) Console.Write($"\"{strs[i]}\"({(i + 1 == size ? "]" : ",")})");
-			Console.WriteLine();
 			string common = string.Empty;
+
+			Console.Write("Input: strs = [");
+			for (int i = 0; i < size; i++) Console.Write($"\"{strs[i]}\"{(i + 1 == size ? "]" : ",")}");
+			Console.WriteLine();
 
 			if (size == 1)
 				common = strs[0];
 			else if(size > 1) {
 				string[] ordered = strs.ToList().OrderBy(x => x.Length).ToArray();
-				foreach (var a in ordered)
-					Console.Write($" [{a}]({a.Length})");
-				Console.WriteLine();
 
-				int i0 = 0;
-				for (int i = 0; i < size; i++)
-					if (ordered[i].Length > 0) {
-						i0 = i; break;
-					}
-
-				Console.WriteLine($"first non-zero length term [{i0}]");
-
-				if (i0 + 1 == size)
-					common = ordered[i0];
-				else if (i0 == 0) {
-					int n = ordered[0].Length;
+				if (ordered[0].Length > 0) {
 					bool found = false;
-					Console.WriteLine($"n ({n}), found ({found})");
-					for(int i = 1; i < n && !found; i++)
-						for(int j = i0 + 1; j < size; j++) {
-							if(ordered[i0].Substring(0, i) != ordered[j].Substring(0, i)){
-								common = ordered[i0].Substring(0, i - 1);
+					for(int i = 1; i < ordered[0].Length && !found; i++)
+						for(int j = 1; j < size; j++)
+							if(ordered[0].Substring(0, i) != ordered[j].Substring(0, i)){
+								common = ordered[0].Substring(0, i - 1);
 								found = true;
 								break;
 							}
-						}
 				}
 			}
 
