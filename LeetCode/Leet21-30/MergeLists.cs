1ff2d62e38c318defb95985d6ccbb644294c372e
Wei Ding <ding_web@yahoo.co.jp>
2021-04-15 02:43:32 -0400

    display

--- LeetCode/Leet21-30/MergeLists.cs
+++ LeetCode/Leet21-30/MergeLists.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+*/
+
+using System;
+using System.Text;
+using System.Linq;
 
 namespace LeetCode.Leet21_30 {
 	public class MergeLists {
@@ -7,8 +13,25 @@ namespace LeetCode.Leet21_30 {
 		}
 
 		private void Action(int[] l1, int[] l2) {
-			Console.WriteLine("Input:");
-			Console.WriteLine("Output:");
+			Console.WriteLine($"Input:{Display(l1, "l1")}, {Display(l2, "l2")}");
+
+			int[] merged = new int[0];
+			Console.WriteLine($"Output:{Display(merged, string.Empty)}");
 		}
+
+		private string Display(int[] nums, string name) {
+			StringBuilder b = new StringBuilder();
+			if(!string.IsNullOrEmpty(name))
+				b.Append($" {name} =");
+			b.Append(" [");
+
+			int n = nums.Length;
+			if(n > 0)
+				for (int i = 0; i < n; i++) b.Append($"{nums[i]}{(i + 1 == n ? "" : ",")}");
+			b.Append("]");
+
+			return b.ToString();
+		}
+
 	}
 }
