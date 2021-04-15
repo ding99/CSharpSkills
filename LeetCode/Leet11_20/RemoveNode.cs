71a838b1f543c077670f722775345f6022ae7302
Wei Ding <ding_web@yahoo.co.jp>
2021-04-15 01:57:08 -0400

    comment

--- LeetCode/Leet11_20/RemoveNode.cs
+++ LeetCode/Leet11_20/RemoveNode.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Follow up: Could you do this in one pass?
+*/
+
+using System;
 using System.Collections.Generic;
 
 namespace LeetCode.Leet11_20 {
