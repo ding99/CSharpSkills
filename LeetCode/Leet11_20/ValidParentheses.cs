86189bd9d5fd78a63280ae4be14d082d4cddb48c
Wei Ding <ding_web@yahoo.co.jp>
2021-04-15 02:02:17 -0400

    start class

---
+++ LeetCode/Leet11_20/ValidParentheses.cs
@@ -0,0 +1,21 @@
+﻿/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.Leet11_20 {
+	public class ValidParentheses {
+		public void Start() {
+
+		}
+	}
+}
