4a1ce28f30339a8b16dec33ac20b9cb858fa9170
Wei Ding <ding_web@yahoo.co.jp>
2021-04-15 01:45:37 -0400

    one test

--- LeetCode/Leet11_20/RemoveNode.cs
+++ LeetCode/Leet11_20/RemoveNode.cs
@@ -4,6 +4,17 @@ using System.Text;
 using System.Linq;
 
 namespace LeetCode.Leet11_20 {
+
+	internal class Node {
+		internal int value;
+		internal Node next;
+
+		public Node(int d) {
+			value = d;
+			next = null;
+		}
+	}
+
 	public class RemoveNode {
 		public void Start() {
 			Action(new int[] { 1, 2, 3, 4, 5}, 2);
@@ -13,7 +24,51 @@ namespace LeetCode.Leet11_20 {
 			int size = Display(array, "nums");
 			Console.WriteLine($", n = {n}");
 
-			int[] result = new int[] { 1, 2 };
+			int[] result = new int[0];
+			if (size >= n) {
+				Node head = null, curt = null, next = null;
+				for (int i = 0; i < size; i++) {
+					next = new Node(array[i]);
+					if (curt == null) head = next;
+					else curt.next = next;
+					curt = next;
+				}
+
+				int count = 0;
+				for (curt = head; curt != null; curt = curt.next)
+					count++;
+				Console.WriteLine($"count 1 [{count}]");
+				for (curt = head; curt != null; curt = curt.next)
+					Console.Write(curt.value + " ");
+				Console.WriteLine();
+
+				next = null;
+				for (curt = head; curt != null; next = curt, curt = curt.next) {
+					if(count-- == n) {
+						Console.WriteLine($"found: {curt.value} count {count}");
+						if (next == null)
+							head = curt.next;
+						else
+							next.next = curt.next;
+						break;
+					}
+				}
+
+				count = 0;
+				for (curt = head; curt != null; curt = curt.next)
+					count++;
+				Console.WriteLine($"count 2 [{count}]");
+				for (curt = head; curt != null; curt = curt.next)
+					Console.Write(curt.value + " ");
+				Console.WriteLine();
+
+				List<int> values = new List<int>();
+				for (curt = head; curt != null; curt = curt.next)
+					values.Add(curt.value);
+
+				result = values.ToArray();
+			}
+
 			Display(result, string.Empty);
 			Console.WriteLine();
 		}
