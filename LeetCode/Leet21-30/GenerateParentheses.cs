89b9df3c9389b06755fd999e38dcded3e9a85c0d
Wei Ding <ding_web@yahoo.co.jp>
2021-04-17 00:20:26 -0400

    menu

---
+++ LeetCode/Leet21-30/GenerateParentheses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Leet21_30 {
+	public class GenerateParentheses {
+		public void Start() {
+			Action(3);
+		}
+
+		private void Action(int n) {
+			Console.WriteLine($"Input: n = {n}");
+			string[] result = new string[] { "()()", "[][]" };
+
+			StringBuilder b = new StringBuilder("[");
+			for (int i = 0; i < result.Length; i++)
+				b.Append($"{result[i]}{(i + 1 == result.Length ? "" : ",")}");
+			b.Append("]");
+			Console.Write($"Output: {b}");
+		}
+	}
+}
