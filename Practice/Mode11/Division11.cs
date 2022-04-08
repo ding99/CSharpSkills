8b208a3420e7a03ae00074f62c7c3781d2383579
Wei Ding <ding_web@yahoo.co.jp>
2022-04-07 23:34:50 -0700

    mode 11

---
+++ Practice/Mode11/Division11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mode11
+{
+    public class Division11
+    {
+        public void Start ()
+        {
+            int n = 0;
+            for(int i = 10000; i < 100000; i++)
+            {
+                if (this.Exact (i))
+                {
+                    Console.Write ($"{i} ");
+                    n++;
+                }
+            }
+            Console.WriteLine ();
+            Console.WriteLine (n);
+        }
+
+
+        private bool Exact(int n)
+        {
+            int newOne = (n / 100) + n % 100;
+            return newOne % 11 == 0;
+        }
+    }
+}
---
+++ Practice/Mode11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mode11
+{
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine ("== Start");
+
+            new Division11 ().Start ();
+
+            Console.WriteLine ("== End");
+        }
+    }
+}
